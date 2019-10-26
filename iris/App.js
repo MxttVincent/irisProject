54bc4bcc8e4f64106c11c122c647d97204c03d73
Kimo <jm865@kent.ac.uk>
2019-10-26 20:57:31 +0100

    Installed & configured firebase. setup working sign up method

--- iris/App.js
+++ iris/App.js
@@ -1,34 +1,63 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { text: ''}
+    this.state = { 
+      email: '',
+      password: ''
+    }
   }
 
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBsDGLvWJTHpFBf2pUZoNL9QEfmPe81Sz4",
+      authDomain: "iris-pro.firebaseapp.com",
+      databaseURL: "https://iris-pro.firebaseio.com",
+      projectId: "iris-pro",
+      storageBucket: "iris-pro.appspot.com",
+      messagingSenderId: "239445951991",
+      appId: "1:239445951991:web:9b035888b4ac6f057fdaf2",
+      measurementId: "G-DQFV89EYG1"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+  }
+  
   render() {
     
+
+    signUpUser = (email, password) => {
+      console.log("user was created");
+      firebase.auth().createUserWithEmailAndPassword(email, password);
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <TextInput 
         style={styles.input}
         placeholder="email"
-        value={this.state.text} 
-        onChangeText={(text) => this.setState({text})} 
+        value={this.state.email} 
+        onChangeText={(email) => this.setState({email})} 
         />
         <TextInput 
         style={styles.input}
         placeholder="password"
         secureTextEntry={true}
-        value={this.state.text} 
-        onChangeText={(text) => this.setState({text})} 
+        value={this.state.password} 
+        onChangeText={(password) => this.setState({password})} 
         />
         <Button
           title="Sign up"
-          onPress={() => this.signUpUser(this.state.email, this.state.password)}
+          onPress={() => signUpUser(this.state.email, this.state.password)}
         />
       </View>
     );
