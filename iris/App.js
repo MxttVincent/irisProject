e0aecd182cac040148682c05f7e9e5777ff87ece
Kimo <jm865@kent.ac.uk>
2019-10-28 23:08:24 +0000

    Fixed email-auth file to latest version

--- iris/App.js
+++ iris/App.js
@@ -29,19 +29,81 @@ export default class App extends React.Component {
       measurementId: "G-DQFV89EYG1"
     };
     firebase.initializeApp(firebaseConfig);
-
+    
+    // For each of your app's pages that need information about the signed-in user, 
+    // attach an observer to the global authentication object. 
+    // This observer gets called whenever the user's sign-in state changes.
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        console.log("user is logged in");
+        // console.log([displayName, email, emailVerified, photoURL, isAnonymous, uid, providerData]);
+        // ...
+      } else {
+        // User is signed out.
+        // ...
+        console.log("user is not logged in");
+      }
+    });
+    
   }
   
   render() {
     
-
-    signUpUser = (email, password) => {
-      console.log("user was created");
-      firebase.auth().createUserWithEmailAndPassword(email, password);
+    onSignUp = (email, password) => {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        console.log("user was created");
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+  
+        if (errorCode == 'auth/invalid-email') {
+          alert(errorMessage);
+        }
+        if(errorCode == 'auth/weak-password') {
+          alert(errorMessage)
+        }
+        if(errorCode =='auth/email-already-in-use') {
+          alert(errorMessage);
+        }
+      });
     }
+
+    onSignIn = (email, password) => {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+        console.log("user signed in");
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error.code)
+        if(errorCode == 'auth/user-not-found') {
+          alert(errorMessage);
+        }
+        if(errorCode == 'auth/wrong-password') {
+          alert(errorMessage);
+        }
+        // disabled account for signing in
+        if(errorCode == 'auth/user-disabled') {
+          alert(errorMessage);
+        }
+      });
+    }
+    // signs out the current user
+    onSignOut = () => {
+      firebase.auth().signOut();
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
         <TextInput 
         style={styles.input}
         placeholder="email"
@@ -57,7 +119,15 @@ export default class App extends React.Component {
         />
         <Button
           title="Sign up"
-          onPress={() => signUpUser(this.state.email, this.state.password)}
+          onPress={() => onSignUp(this.state.email, this.state.password)}
+        />
+        <Button
+          title="Sign in"
+          onPress={() => onSignIn(this.state.email, this.state.password)}
+        />
+        <Button
+          title="Sign out"
+          onPress={() => onSignOut()}
         />
       </View>
     );
