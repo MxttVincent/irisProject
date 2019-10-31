1efca48d49ff713fa002a47ea35cad006d73af29
Kimo <jm865@kent.ac.uk>
2019-10-31 00:20:21 +0000

    Added basic components to sign in screen

--- iris/src/screens/SignIn.js
+++ iris/src/screens/SignIn.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 class SignIn extends React.Component {
   render() {
     return (
-      <Text> This is the sign in screen</Text>
+      <View>
+        <TextInput 
+        style={styles.input}
+        placeholder="Email/Username"
+        />
+        <TextInput 
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        />
+        <TouchableOpacity
+          onPress={() => alert('Forgot password has been pressed')}>
+            <Text style={styles.textLink}>Forgot Password</Text>
+        </TouchableOpacity>
+        <Button
+          title="Sign in"
+          onPress={() => alert('Sign in pressed')}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1 
+  },
+  textLink: {
+    textDecorationLine: 'underline',
+    color: 'gray'
+  }
+})
 
 export default SignIn;
