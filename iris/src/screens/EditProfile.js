6d2711cd3e2dd11ff919792af6313e627344780a
Kimo <jm865@kent.ac.uk>
2019-11-02 23:42:31 +0000

    setup EditProfile screen & basic functions

--- iris/src/screens/EditProfile.js
+++ iris/src/screens/EditProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, View, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import firebase from '../config/firebase';
 
 export default class EditProfile extends React.Component {
   static navigationOptions = {
@@ -7,9 +8,81 @@ export default class EditProfile extends React.Component {
     /* No more header config here! */
   };
 
+  constructor(props) {
+    super(props);
+    const user = firebase.auth().currentUser;
+    let username, email, photoUrl, uid, emailVerified;
+
+    this.state = {
+      username: user.displayName,
+      email: user.email,
+      photoUrl: user.photoURL,
+
+    }
+  }
+
+  onSaveChanges = () => {
+    console.log("saving changes");
+  }
+
+  onChangePassword = () => {
+    console.log("change password was pressed");
+  }
+
   render() {
+    // console.log(user);
+
+    // if (user != null) {
+    //   username = user.displayName;
+    //   email = user.email;
+    //   photoUrl = user.photoURL;
+    //   emailVerified = user.emailVerified;
+    //   uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+    //                   // this value to authenticate with your backend server, if
+    //                   // you have one. Use User.getToken() instead.
+    // }
     return (
-      <Text>Edit profile screen</Text>
+      <View>
+        <Text>Edit profile screen</Text>
+          <TextInput 
+          style={styles.input}
+          placeholder="Username"
+          value={this.state.username} 
+          onChangeText={(username) => this.setState({username})} 
+          />
+          <TextInput 
+          style={styles.input}
+          placeholder="Email"
+          value={this.state.email} 
+          onChangeText={(email) => console.log("")} 
+          />
+          <TextInput 
+          style={styles.input}
+          placeholder="extra field"
+          value={this.state.username} 
+          onChangeText={(username) => console.log("")} 
+          />
+          <Button 
+          title="Save Changes"
+          onPress={() => this.onSaveChanges()}
+          />
+          <TouchableOpacity
+          onPress={() => this.onChangePassword()}>
+            <Text style={styles.textLink}>Change Password</Text>
+          </TouchableOpacity>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1 
+  },
+  textLink: {
+    textDecorationLine: 'underline',
+    color: 'gray'
+  }
+})
