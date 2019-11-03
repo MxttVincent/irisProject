42ceb0aebe2b7d7b33535274dc1ec46e9deb8e20
Kimo <jm865@kent.ac.uk>
2019-11-03 20:14:03 +0000

    no message

---
+++ iris/src/config/firebase.js
@@ -0,0 +1,22 @@
+// setup for firebase products
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsDGLvWJTHpFBf2pUZoNL9QEfmPe81Sz4",
+  authDomain: "iris-pro.firebaseapp.com",
+  databaseURL: "https://iris-pro.firebaseio.com",
+  projectId: "iris-pro",
+  storageBucket: "iris-pro.appspot.com",
+  messagingSenderId: "239445951991",
+  appId: "1:239445951991:web:9b035888b4ac6f057fdaf2",
+  measurementId: "G-DQFV89EYG1"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
