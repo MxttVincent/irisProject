a832169133433674efc36c0495b8f8596ec3b2d9
Kimo <jm865@kent.ac.uk>
2019-11-26 20:47:01 +0000

    Added fetch photos to display photos in the cloud. Need to fix display async version

--- iris/src/screens/Gallery.js
+++ iris/src/screens/Gallery.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 import firebase from '../config/firebase';
@@ -10,10 +10,6 @@ var storage = firebase.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.ref();
 
-// Create a child reference
-var imagesRef = storageRef.child('images/test.jpg');
-// imagesRef now points to 'images'
-
 export default class Gallery extends React.Component {
 
   constructor(props) {
@@ -21,13 +17,16 @@ export default class Gallery extends React.Component {
     this.state = {
       image: null,
       height: null,
-      width: null
+      width: null,
+      uid: firebase.auth().currentUser.uid || null,
+      photos: []
     }
   }
 
   componentDidMount = () => {
-    this.fetchPhotos();
+    this.fetchPhotos(this.state.uid);
   }
+
 
   loadImageLibrary = async () => {
       let result = await ImagePicker.launchImageLibraryAsync({
@@ -47,14 +46,17 @@ export default class Gallery extends React.Component {
       }
   }
 
-  uploadImage = async () => {
+  uploadImage = async (uid) => {
+    // fetch the image uri
     const response = await fetch(this.state.image);
+    // create a BINARY LARGE OBJECT (Blob)
     const blob = await response.blob();
-
+    // store the images filename
     const filename = blob.data.name;
-    let imagesRef = storageRef.child(`images/${filename}`); 
+    // create a reference where the image will live in storage
+    let imagesRef = storageRef.child(`users/${uid}/${filename}`); 
+    // Upload the blob/image to the referred location
     imagesRef.put(blob).then((snapshot) => {
-      // console.log(snapshot);
       console.log("image uploaded successfully");
       this.setState({
         image: null,
@@ -67,11 +69,24 @@ export default class Gallery extends React.Component {
     
   }
 
-  fetchPhotos = () => {
-    const listRef = storageRef.child('images');
-    // console.log(listRef);
+  fetchPhotos = (uid) => {
+    const listRef = storageRef.child(`users/${uid}`);
+    listRef.listAll().then(res => {
+      res.items.forEach(itemRef => {
+        itemRef.getDownloadURL().then(url => {
+          this.setState({photos: [...this.state.photos, url]});
+        }).catch(error => {
+          console.log(error);
+        }) 
+
+      })
+    }).catch(err => {
+      console.log(err);
+    })
     
   }
+
+  
 
   render() {
     return (
@@ -81,14 +96,17 @@ export default class Gallery extends React.Component {
           : <Text>No Image Selected, please select an image</Text> }
 
         {this.state.image 
-          ? <Button title="Upload your selected photo to storage" onPress={() => this.uploadImage()}/>
+          ? <Button title="Upload your selected photo to storage" onPress={() => this.uploadImage(this.state.uid)}/>
           : <Button title="Pick an image from your library" onPress={() => this.loadImageLibrary()} />
         }
 
-        
-        
         <View style={styles.photoArea}>
           <Text>current photos in the cloud: </Text>
+          {this.state.photos.map(photo =>{ 
+              return (
+                <Image key={photo} source={{uri: photo}} style={{width: 100, height: 100}}/>
+              )
+          })}
         </View>
       </View>
     )
