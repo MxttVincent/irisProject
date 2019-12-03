cab04a442ac0d20846f3ed7808925c5d5a2d654c
Matt <mv273@kent.ac.uk>
2019-12-03 11:51:53 +0000

    added correct studio code

--- iris/src/screens/Studio.js
+++ iris/src/screens/Studio.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react'
-import { Text, TextInput, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, Button, TouchableOpacity, Image, CameraRoll} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 import firebase from '../config/firebase';
+
 export default class Studio extends Component {
+    
+    static navigationOptions = {
+        title: 'Studio',
+    };
+
+    state = {
+        image: null,
+        height: null,
+        width: null
+    };
+
+    componentDidMount() {
+        this.getPermissionAsync();
+    }
+
+    getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if (status !== 'granted') {
+            alert('Please allow access to Gallery');
+        }
+        }
+    }
+
+    openGallery = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            //Only allow images to be selected
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: false,
+          aspect: [4, 3],
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          this.setState({ 
+              image: result.uri,
+              height: result.height,
+              width: result.width
+        });
+
+        const { img } = this.state;
+
+        this.props.navigation.navigate('Editor', {result});
+    }
+    }
+    
     render() {
         return (
             <View>
@@ -15,7 +66,11 @@ export default class Studio extends Component {
             onPress={() => this.props.navigation.navigate('Camera')}/>
             <Button
             title="Select Image"
-            onPress={() => this.props.navigation.navigate('ImageSelector')}/>
+            onPress={() => {
+                this.openGallery();
+                /* this.props.navigation.navigate('Editor', { img }); */
+            }
+        }/>
             </View>
             
         )
