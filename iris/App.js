60bb0c6d67abff0dfe9e36b6794e943900f7343b
Matt <mv273@kent.ac.uk>
2019-12-03 12:01:30 +0000

    .

--- iris/App.js
+++ iris/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createRootNavigator } from "./src/router"; // import stack & tab navigation
 import firebase from './src/config/firebase';
-// import screens
-import SignIn from './src/screens/SignIn';
-import SignUp from './src/screens/SignUp';
-import Profile from './src/screens/Profile';
-import EditProfile from './src/screens/EditProfile';
-import Camera from './src/screens/Camera';
-import Studio from './src/screens/Studio';
-import ImageSelector from './src/screens/ImageSelector'
 
- class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home Screen',
-    /* No more header config here! */
-    
-  };
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: false,
+    }
+  }
 
   componentDidMount() {
     // For each of your app's pages that need information about the signed-in user, 
@@ -26,80 +18,29 @@ import ImageSelector from './src/screens/ImageSelector'
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
+        this.setState({signedIn: true});
         console.log("user is logged in");
+        
         // if user is logged in, navigate them to the appropriate page
       } else {
         // User is signed out.
         // ...
+        this.setState({signedIn: false});
         console.log("user is not logged in");
       }
     });
   }
 
   render() {
+    // const A = createAppContainer(createRootNavigator(false));
+  const A = createAppContainer(createRootNavigator(this.state.signedIn));
+
+    console.log(this.state.signedIn);
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Updated version 0.2.0</Text>
-        <Button
-        title="Go to sign in"
-        onPress={() => this.props.navigation.navigate('SignIn')}/>
-        <Button
-        title="Go to sign up with email"
-        onPress={() => this.props.navigation.navigate('SignUp')}/>
-        <Button
-        title="Go to profile screen"
-        onPress={() => {
-          // must be logged in to go to profile screen
-          if (firebase.auth().currentUser) {
-            this.props.navigation.navigate('Profile');
-          } else {
-            alert("user is not logged in.");
-          }
-          }
-        }
-        />
-        <Button
-        title="sign out"
-        onPress={() => firebase.auth().signOut()} 
-        />
-        <Button
-        title="Go to Studio"
-        onPress={() => this.props.navigation.navigate('Studio')}/>
-      </View>
+      <A />
     )
   }
+
 }
 
-const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    SignIn,
-    SignUp,
-    Profile,
-    EditProfile,
-    Camera,
-    Studio,
-    ImageSelector
-  },
-  {
-    defaultNavigationOptions: {
-      headerTintColor: '#121212',
-      headerStyle: {
-        backgroundColor: '#fff',
-      },
-    },
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
-
-export default createAppContainer(AppNavigator);
 
