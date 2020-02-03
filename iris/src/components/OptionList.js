e9af091efd10aa6bfc26af6d96717531735a2ed0
Kimo <jm865@kent.ac.uk>
2020-02-03 22:04:37 +0000

    Added state to OptionList. Added iconPress handler to update state. Made OptionList render conditionally, depending on state

--- iris/src/components/OptionList.js
+++ iris/src/components/OptionList.js
@@ -6,41 +6,75 @@ import AdjusmentOption from './AdjusmentOption';
 import AdjustmentsOptionData from './options.json';
 import filterOptions from './filterOptions.json';
 
+import { Slider } from 'react-native-elements'
+
 // Renders a list of options for EditorScreen.
 export default class OptionList extends React.Component {
+  constructor(props) {
+    super(props);
 
-    renderOptions = (type) => {
-    switch (type) {
-      case 'filter':
-        return <FlatList data={filterOptions} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={(obj, index) => index.toString()} renderItem={(obj) => (
-          <TouchableOpacity style={{flex: 1, flexDirection: 'row'}} onPress={() => console.log('option clicked')}>
-            <AdjusmentOption text={obj.item.text || null} iconName={obj.item.iconName || null} />
-          </TouchableOpacity>
-        )
-      }/>
-      case 'sliders':
-        return <FlatList data={AdjustmentsOptionData} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={(obj, index) => index.toString()} renderItem={(obj) => (
-                <TouchableOpacity style={{flex: 1, flexDirection: 'row'}} onPress={() => console.log('option clicked')}>
-                  <AdjusmentOption text={obj.item.text || null} iconName={obj.item.iconName || null} />
-                </TouchableOpacity>
-              )
-        }/>
-      case 'history':
-        return <FlatList data={AdjustmentsOptionData} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={(obj, index) => index.toString()} renderItem={(obj) => (
-                <TouchableOpacity style={{flex: 1, flexDirection: 'row'}} onPress={() => console.log('option clicked')}>
-                  <AdjusmentOption text={obj.item.text || null} iconName={obj.item.iconName || null} onPress={() => console.log('option clicked')}/>
-                </TouchableOpacity>
-              )
-      }/>
-      default:
-        return null;
+    this.state = {
+      contrast: 1,
+      isSliderActive: false,
+      areOptionsShowing: true
     }
   }
 
+  handleIconPress = () => {
+    this.setState({isSliderActive: true, areOptionsShowing: false})
+  }
+  
+
+renderOptions = (type) => {
+switch (type) {
+  case 'filter':
+    return <FlatList data={filterOptions} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={(obj, index) => index.toString()} renderItem={(obj) => (
+      <TouchableOpacity style={{flex: 1, flexDirection: 'row'}} onPress={() => console.log('option clicked')}>
+        <AdjusmentOption text={obj.item.text || null} iconName={obj.item.iconName || null} />
+      </TouchableOpacity>
+    )
+  }/>
+  case 'sliders':
+    return <FlatList data={AdjustmentsOptionData} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={(obj, index) => index.toString()} renderItem={(obj) => (
+            <TouchableOpacity style={{flex: 1, flexDirection: 'row'}} onPress={() => this.handleIconPress()}>
+              <AdjusmentOption text={obj.item.text || null} iconName={obj.item.iconName || null} />
+            </TouchableOpacity>
+          )
+    }/>
+  case 'history':
+    return <FlatList data={AdjustmentsOptionData} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={(obj, index) => index.toString()} renderItem={(obj) => (
+            <TouchableOpacity style={{flex: 1, flexDirection: 'row'}} onPress={() => console.log('option clicked')}>
+              <AdjusmentOption text={obj.item.text || null} iconName={obj.item.iconName || null} onPress={() => console.log('option clicked')}/>
+            </TouchableOpacity>
+          )
+  }/>
+  default:
+    return null;
+  
+       }
+      }
+    
+
   render() {
-    // this.props.type will tell option list which list to render
     return (
-      this.renderOptions(this.props.type)
+      <View>
+        {
+            this.state.areOptionsShowing 
+          ? this.renderOptions(this.props.type) 
+          : null
+        }
+        
+        {
+          this.state.isSliderActive ? <Slider
+          value={this.state.contrast}
+          onValueChange={value => this.setState({ contrast: value })}
+          />
+          : 
+          null
+        }
+
+      </View>
+
       
     )
     
