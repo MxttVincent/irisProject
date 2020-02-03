f3d2e7971cb7075d11231eaecf260848053c822c
Kimo <jm865@kent.ac.uk>
2020-02-03 22:03:33 +0000

    Added isShowing prop to the optionList component in Scroller

--- iris/src/components/Scroller.js
+++ iris/src/components/Scroller.js
@@ -4,14 +4,13 @@ import { ScrollView, Text, View } from 'react-native';
 import OptionList from './OptionList';
 
 // can be deleted if it seems like we only need option list, which so far looks so.
-export default Scroller = (props) => {
-  const {type, active} = props;
+export default Scroller = ({type, isShowing}) => {
   // show only return content if type is defined.  !undefined. null if so
   // Depending on the type return different options
   // Each Option needs their own icon and text value, as well as an on press function. 
   return (
       <View>
-        <OptionList type={type}/>
+        <OptionList type={type} isShowing={isShowing}/>
       </View>
   )
 }
