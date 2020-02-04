5f154fadecf64bf4301a9111b01fffeb60ec86bf
Kimo <jm865@kent.ac.uk>
2020-02-04 19:10:24 +0000

    Added Conditional Rendering into Scroller. To show either a list of options(icons) or a slider component

--- iris/src/components/Scroller.js
+++ iris/src/components/Scroller.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import React from 'react';
+import {View } from 'react-native';
 
 import OptionList from './OptionList';
+import { Slider } from 'react-native-elements'
 
-// can be deleted if it seems like we only need option list, which so far looks so.
-export default Scroller = ({type, isShowing}) => {
+
+export default Scroller = ({type, areOptionsShowing, iconPressHandler}) => {
   // show only return content if type is defined.  !undefined. null if so
   // Depending on the type return different options
   // Each Option needs their own icon and text value, as well as an on press function. 
   return (
       <View>
-        <OptionList type={type} isShowing={isShowing}/>
+      {
+        areOptionsShowing 
+        ? <OptionList type={type} iconPressHandler={iconPressHandler}/> 
+        : <Slider value={1} onValueChange={value => console.log(value)}/>
+      }
       </View>
   )
 }
