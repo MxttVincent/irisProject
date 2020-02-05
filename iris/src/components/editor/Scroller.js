e215349f08430b7762838774948e2f7df1afa2f4
Kimo <jm865@kent.ac.uk>
2020-02-05 20:00:20 +0000

    Added prop function to slider when value changes. value is 1 just for now

--- iris/src/components/editor/Scroller.js
+++ iris/src/components/editor/Scroller.js
@@ -5,7 +5,7 @@ import OptionList from './OptionList';
 import { Slider } from 'react-native-elements'
 
 
-export default Scroller = ({type, areOptionsShowing, iconPressHandler, closeSlider}) => {
+export default Scroller = ({type, areOptionsShowing, iconPressHandler, handleSliderChange,closeSlider, imgPropertyValues}) => {
   // show only return content if type is defined.  !undefined. null if so
   // Depending on the type return different options
   // Each Option needs their own icon and text value, as well as an on press function. 
@@ -17,7 +17,7 @@ export default Scroller = ({type, areOptionsShowing, iconPressHandler, closeSlid
         : <View><TouchableOpacity onPress={closeSlider}>
         <Text>close</Text>
         </TouchableOpacity>
-        <Slider value={1} onValueChange={value => console.log(value)}/>
+        <Slider value={1} onValueChange={value => handleSliderChange(value)}/>
         </View>
       }
       </View>
